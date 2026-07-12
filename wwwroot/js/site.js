b1d597a5fdd3f3d2c686321bc4ffbf692d8cafec
Jarod <jarodoffice@gmail.com>
2026-07-12 18:14:51 +0800

    Applied bootstrap and updated the folder structure

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,15 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-document.addEventListener('DOMContentLoaded', function () {
-  var navToggle = document.getElementById('navToggle');
-  var siteNav = document.getElementById('siteNav');
-
-  if (navToggle && siteNav) {
-    navToggle.addEventListener('click', function () {
-      var isOpen = siteNav.classList.toggle('is-open');
-      navToggle.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
-    });
-  }
-});
